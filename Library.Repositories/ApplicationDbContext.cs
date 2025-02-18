73b81713a710e18111fa1f12757de36cc6989f51
Anjalee Weerasinghe <anjalee362@gmail.com>
2025-02-18 13:58:13 +0530

    Add project files.

---
+++ Library.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Library.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Repositories
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
