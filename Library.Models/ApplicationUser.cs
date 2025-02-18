d5dbe9b78d3dc3a81351d6188699639dc1cd4daa
Anjalee Weerasinghe <anjalee362@gmail.com>
2025-02-18 14:38:52 +0530

    Initiate

--- Library.Models/ApplicationUser.cs
+++ Library.Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,19 @@ namespace Library.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public string Nationality { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
+
+namespace Library.Models
+{
+    public enum Gender
+    {
+        Male, Female, Other
     }
 }
