516a805a3f1e451fed871e9260a98ecb2d62e52e
Anjalee Weerasinghe <anjalee362@gmail.com>
2025-06-21 04:38:01 +0530

    Create Library Member Module

--- Library.ViewModels/MemberViewModel.cs
+++ Library.ViewModels/MemberViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,34 @@ namespace Library.ViewModels
         public int UserId { get; set; }
         public string MemberCode { get; set; }
         public DateTime MembershipDate { get; set; }
+        public int? MembershipTypeId { get; set; }
+
+        public MemberViewModel()
+        {
+        }
+
+        public MemberViewModel(Member model)
+        {
+            Id = model.Id;
+            UserId = model.UserId;
+            MemberCode = model.MemberCode;
+            MembershipDate = model.MembershipDate;
+            MembershipTypeId = model.MembershipTypeId;
+        }
+
+        public Member ConvertViewModel(MemberViewModel model)
+        {
+            return new Member
+            {
+                Id = model.Id,
+                UserId = model.UserId,
+                MemberCode = model.MemberCode,
+                MembershipDate = model.MembershipDate,
+                MembershipTypeId = model.MembershipTypeId
+
+            };
+        }
+
+
     }
 }
